cf37ddf65d90b0f85dc243e4b181d39f6d1310cd
andy8052 <andychorlian@gmail.com>
2020-07-15 14:25:18 -0400

    Initial commit

--- src/2020-07-27-Kovan-DssSpell.sol
+++ src/2020-07-27-Kovan-DssSpell.sol
@@ -44,10 +44,11 @@ contract SpellAction {
     address constant public MCD_END             = 0x24728AcF2E2C403F5d2db4Df6834B8998e56aA5F;
     address constant public FLIPPER_MOM         = 0xf3828caDb05E5F22844f6f9314D99516D68a0C84;
 
-    // LEND specific addresses
-    address constant public MCD_JOIN_LEND_A     = 0x0Dd1a7BD9BB1E81935AC128D9af48e1288cBb2f8;
-    address constant public PIP_LEND            = 0x03E4D947888AADC7ab2BB39785EE806274d46810;
-    address constant public MCD_FLIP_LEND_A     = 0xf97CDb0432943232B0b98a790492a3344eCB5256;
+    // MANA specific addresses
+    // MANA token address 0x221f4d62636b7b51b99e36444ea47dc7831c2b2f
+    address constant public MCD_JOIN_MANA_A     = 0xd4af9cd0d9e7b98890875282ef2554b6c1a26c77;
+    address constant public PIP_MANA            = 0xE97D2b077Fe19c80929718d377981d9F754BF36e;
+    address constant public MCD_FLIP_MANA_A     = 0x392f8559f3a3bb39dd917eb8db115f61a3b9208b;
 
     // decimals & precision
     uint256 constant public THOUSAND            = 10 ** 3;
@@ -62,7 +63,7 @@ contract SpellAction {
     //
     // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
     //
-    uint256 constant public SIX_PCT_RATE        = 1000000001847694957439350562;
+    uint256 constant public TWELVE_PCT_RATE        = 1000000003593629043335673582;
 
     function execute() external {
 
@@ -83,47 +84,47 @@ contract SpellAction {
         // VatAbstract(MCD_VAT).file("Line", 246 * MILLION * RAD);
 
         ////////////////////////////////////////////////////////////////////////////////
-        // LEND-A 
+        // MANA-A 
 
         // set ilk bytes32 variable
-        bytes32 constant LEND_A_ILK = "LEND-A";
+        bytes32 constant MANA_A_ILK = "MANA-A";
 
         // Init USDC-B in Vat & Jug
-        VatAbstract(MCD_VAT).init(LEND_A_ILK);
-        JugAbstract(MCD_JUG).init(LEND_A_ILK);
+        VatAbstract(MCD_VAT).init(MANA_A_ILK);
+        JugAbstract(MCD_JUG).init(MANA_A_ILK);
 
-        // Allow LEND-A Join to modify Vat registry
-        VatAbstract(MCD_VAT).rely(MCD_JOIN_LEND_A);
+        // Allow MANA-A Join to modify Vat registry
+        VatAbstract(MCD_VAT).rely(MCD_JOIN_MANA_A);
 
-        // set price feed for LEND-A
-        SpotAbstract(MCD_SPOT).file(LEND_A_ILK, "pip", PIP_USDC);
+        // set price feed for MANA-A
+        SpotAbstract(MCD_SPOT).file(MANA_A_ILK, "pip", PIP_USDC);
 
         // set the USDC-B flipper in the cat
-        CatAbstract(MCD_CAT).file(LEND_A_ILK, "flip", MCD_FLIP_LEND_A);
+        CatAbstract(MCD_CAT).file(MANA_A_ILK, "flip", MCD_FLIP_MANA_A);
 
         // Allow cat to kick auctions in USDC-B Flipper 
         // NOTE: this will be reverse later in spell, and is done only for explicitness.
-        FlipAbstract(MCD_FLIP_LEND_A).rely(MCD_CAT);
+        FlipAbstract(MCD_FLIP_MANA_A).rely(MCD_CAT);
 
         // Allow End to yank auctions in USDC-B Flipper
-        FlipAbstract(MCD_FLIP_LEND_A).rely(MCD_END);
+        FlipAbstract(MCD_FLIP_MANA_A).rely(MCD_END);
 
         // Allow FlipperMom to access the USDC-B Flipper
-        FlipAbstract(MCD_FLIP_LEND_A).rely(FLIPPER_MOM);
+        FlipAbstract(MCD_FLIP_MANA_A).rely(FLIPPER_MOM);
 
-        VatAbstract(MCD_VAT).file(LEND_A_ILK,   "line"  , 1 * MILLION * RAD    ); // 1 MM debt ceiling
-        VatAbstract(MCD_VAT).file(LEND_A_ILK,   "dust"  , 20 * RAD             ); // 20 Dai dust
-        CatAbstract(MCD_CAT).file(LEND_A_ILK,   "lump"  , 200 * THOUSAND * WAD ); // 200,000 lot size
-        CatAbstract(MCD_CAT).file(LEND_A_ILK,   "chop"  , 113 * RAY / 100      ); // 13% liq. penalty
-        JugAbstract(MCD_JUG).file(LEND_A_ILK,   "duty"  , SIX_PCT_RATE         ); // 6% stability fee
-        FlipAbstract(MCD_FLIP_LEND_A).file(     "beg"   , 103 * WAD / 100      ); // 3% bid increase
-        FlipAbstract(MCD_FLIP_LEND_A).file(     "ttl"   , 6 hours              ); // 6 hours ttl
-        FlipAbstract(MCD_FLIP_LEND_A).file(     "tau"   , 6 hours              ); // 3 days tau
-        SpotAbstract(MCD_SPOT).file(LEND_A_ILK, "mat"   , 120 * RAY / 100      ); // 120% coll. ratio
-        SpotAbstract(MCD_SPOT).poke(LEND_A_ILK);
+        VatAbstract(MCD_VAT).file(MANA_A_ILK,   "line"  , 1 * MILLION * RAD    ); // 1 MM debt ceiling
+        VatAbstract(MCD_VAT).file(MANA_A_ILK,   "dust"  , 20 * RAD             ); // 20 Dai dust
+        CatAbstract(MCD_CAT).file(MANA_A_ILK,   "lump"  , 500 * THOUSAND * WAD ); // 500,000 lot size
+        CatAbstract(MCD_CAT).file(MANA_A_ILK,   "chop"  , 113 * RAY / 100      ); // 13% liq. penalty
+        JugAbstract(MCD_JUG).file(MANA_A_ILK,   "duty"  , TWELVE_PCT_RATE      ); // 12% stability fee
+        FlipAbstract(MCD_FLIP_MANA_A).file(     "beg"   , 103 * WAD / 100      ); // 3% bid increase
+        FlipAbstract(MCD_FLIP_MANA_A).file(     "ttl"   , 6 hours              ); // 6 hours ttl
+        FlipAbstract(MCD_FLIP_MANA_A).file(     "tau"   , 6 hours              ); // 6 hours tau
+        SpotAbstract(MCD_SPOT).file(MANA_A_ILK, "mat"   , 175 * RAY / 100      ); // 175% coll. ratio
+        SpotAbstract(MCD_SPOT).poke(MANA_A_ILK);
 
         // consequently, deny USDC-A Flipper
-        FlipperMomAbstract(FLIPPER_MOM).deny(MCD_FLIP_LEND_A);
+        FlipperMomAbstract(FLIPPER_MOM).deny(MCD_FLIP_MANA_A);
 
     }
 }
