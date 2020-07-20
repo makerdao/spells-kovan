d3b006dbd431d8e189909122ef0c2bffaae4c75a
wilbarnes <wbarnesf@gmail.com>
2020-07-20 15:32:32 -0400

    removing commented out line

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -6,7 +6,6 @@ set -e
 dapp --use solc:0.5.12 build
 
 # MkrAuthority
-# export DAPP_TEST_ADDRESS=0x6a3AE20C315E845B2E398e68EfFe39139eC6060C
 export DAPP_TEST_ADDRESS=0xdB33dFD3D61308C33C63209845DaD3e6bfb2c674
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
