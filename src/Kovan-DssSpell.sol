bb66df636c8e7dbcd47301e10cbbc3e2f2dac6ca
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-09-02 09:23:43 -0300

    Leave officeHours modifier uncommented and just comment the caller

--- src/Kovan-DssSpell.sol
+++ src/Kovan-DssSpell.sol
@@ -220,14 +220,13 @@ contract DssSpell {
         expiration = now + 30 days;
     }
 
-    // TODO: removing office hours for kovan deploy, fix for mainnet
-    // modifier officeHours {
-    //     uint day = (now / 1 days + 3) % 7;
-    //     require(day < 5, "Can only be cast on a weekday");
-    //     uint hour = now / 1 hours % 24;
-    //     require(hour >= 14 && hour < 21, "Outside office hours");
-    //     _;
-    // }
+    modifier officeHours {
+        uint day = (now / 1 days + 3) % 7;
+        require(day < 5, "Can only be cast on a weekday");
+        uint hour = now / 1 hours % 24;
+        require(hour >= 14 && hour < 21, "Outside office hours");
+        _;
+    }
 
     function schedule() public {
         require(now <= expiration, "This contract has expired");
@@ -237,7 +236,7 @@ contract DssSpell {
     }
 
     // TODO: removing office hours for kovan deploy, fix for mainnet
-    function cast() public {
+    function cast() public /*officeHours*/ {
         require(!done, "spell-already-cast");
         done = true;
         pause.exec(action, tag, sig, eta);
