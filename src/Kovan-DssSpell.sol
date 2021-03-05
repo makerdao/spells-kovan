a848e3b92fed8b36dcd24edc3fd96b5c53835eb2
Brian McMichael <brian@brianmcmichael.com>
2021-03-05 14:39:31 -0500

    Start spell modifications for execlib

--- src/Kovan-DssSpell.sol
+++ src/Kovan-DssSpell.sol
@@ -15,19 +15,17 @@
 
 pragma solidity 0.6.11;
 
-import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
-import "lib/dss-interfaces/src/dapp/DSTokenAbstract.sol";
-import "lib/dss-interfaces/src/dss/ChainlogAbstract.sol";
+import "dss-exec-lib/DssExec.sol";
+import "dss-exec-lib/DssAction.sol";
 
-
-contract SpellAction {
+contract DssSpellAction is DssAction {
     // KOVAN ADDRESSES
     //
     // The contracts in this list should correspond to MCD core contracts, verify
     //  against the current release list at:
     //     https://changelog.makerdao.com/releases/kovan/active/contracts.json
-    ChainlogAbstract constant CHANGELOG =
-        ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+    //ChainlogAbstract constant CHANGELOG =
+    //    ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
 
 
     // decimals & precision
@@ -47,45 +45,12 @@ contract SpellAction {
     //
     //uint256 constant ZERO_PERCENT_RATE = 1000000000000000000000000000;
 
-    function execute() external {
+    function actions() public override {
+
     }
 }
 
-contract DssSpell {
-    ChainlogAbstract constant CHANGELOG =
-        ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
-    DSPauseAbstract public pause =
-        DSPauseAbstract(CHANGELOG.getAddress("MCD_PAUSE"));
-
-    address         public action;
-    bytes32         public tag;
-    uint256         public eta;
-    bytes           public sig;
-    uint256         public expiration;
-    bool            public done;
-
-    string constant public description = "Kovan Spell Deploy";
-
-    constructor() public {
-        sig = abi.encodeWithSignature("execute()");
-        action = address(new SpellAction());
-        bytes32 _tag;
-        address _action = action;
-        assembly { _tag := extcodehash(_action) }
-        tag = _tag;
-        expiration = now + 30 days;
-    }
-
-    function schedule() public {
-        require(now <= expiration, "This contract has expired");
-        require(eta == 0, "This spell has already been scheduled");
-        eta = now + DSPauseAbstract(pause).delay();
-        pause.plot(action, tag, sig, eta);
-    }
-
-    function cast() public {
-        require(!done, "spell-already-cast");
-        done = true;
-        pause.exec(action, tag, sig, eta);
-    }
+contract DssSpell is DssExec {
+    DssSpellAction internal action_ = new DssSpellAction();
+    constructor() DssExec(action_.description(), block.timestamp + 30 days, address(action_)) public {}
 }
