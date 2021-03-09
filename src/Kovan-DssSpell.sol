59286aa006cee35deaf972f4a98571a380509d65
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-03-09 11:42:00 -0300

    SC-9736 MIP45: LINK-A

--- src/Kovan-DssSpell.sol
+++ src/Kovan-DssSpell.sol
@@ -17,10 +17,18 @@ pragma solidity 0.6.11;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
+import "dss-interfaces/dss/ChainlogAbstract.sol";
+import "dss-interfaces/dss/IlkRegistryAbstract.sol";
 
 contract DssSpellAction is DssAction {
 
     string public constant description = "Kovan Spell";
+
+    address constant MCD_DOG         = address(0);
+    address constant MCD_END         = address(0);
+    address constant MCD_ESM         = address(0);
+    // address constant ILK_REGISTRY    = address(0);
+    address constant MCD_CLIP_LINK_A = address(0);
 
     // Turn off office hours
     function officeHours() public override returns (bool) {
@@ -28,8 +36,96 @@ contract DssSpellAction is DssAction {
     }
 
     function actions() public override {
-        DssExecLib.setChangelogAddress("RWA001_A_INPUT_CONDUIT", 0xB944B07EC3B680b2cEA753125667F7663d424DC3);
-        DssExecLib.setChangelogVersion("1.2.9");
+        address MCD_VAT          = DssExecLib.vat();
+        address MCD_CAT          = DssExecLib.cat();
+        address MCD_VOW          = DssExecLib.vow();
+        address MCD_POT          = DssExecLib.pot();
+        address MCD_SPOT         = DssExecLib.spotter();
+        address MCD_END_OLD      = DssExecLib.end();
+        address MCD_ESM_OLD      = DssExecLib.getChangelogAddress("MCD_ESM");
+        address MCD_FLIP_LINK_A  = DssExecLib.flip("LINK-A");
+        address ILK_REGISTRY_OLD = DssExecLib.reg();
+
+        // --------------------  END  --------------------
+
+        // Set contracts in END
+        DssExecLib.setContract(MCD_END,  "vat", MCD_VAT);
+        DssExecLib.setContract(MCD_END,  "cat", MCD_CAT);
+        DssExecLib.setContract(MCD_END,  "dog", MCD_DOG);
+        DssExecLib.setContract(MCD_END,  "vow", MCD_VOW);
+        DssExecLib.setContract(MCD_END,  "pot", MCD_POT);
+        DssExecLib.setContract(MCD_END, "spot", MCD_SPOT);
+
+        // Authorize the new END in contracts
+        DssExecLib.authorize(MCD_VAT, MCD_END);
+        DssExecLib.authorize(MCD_CAT, MCD_END);
+        DssExecLib.authorize(MCD_DOG, MCD_END);
+        DssExecLib.authorize(MCD_VOW, MCD_END);
+        DssExecLib.authorize(MCD_POT, MCD_END);
+        DssExecLib.authorize(MCD_SPOT, MCD_END);
+
+        // Deauthorize the old END in contracts
+        DssExecLib.deauthorize(MCD_VAT, MCD_END_OLD);
+        DssExecLib.deauthorize(MCD_CAT, MCD_END_OLD);
+        DssExecLib.deauthorize(MCD_DOG, MCD_END_OLD);
+        DssExecLib.deauthorize(MCD_VOW, MCD_END_OLD);
+        DssExecLib.deauthorize(MCD_POT, MCD_END_OLD);
+        DssExecLib.deauthorize(MCD_SPOT, MCD_END_OLD);
+
+        // Authorize new ESM to execute in new END
+        DssExecLib.authorize(MCD_END, MCD_ESM);
+
+        // Deuthorize old ESM to execute in old END
+        DssExecLib.deauthorize(MCD_END_OLD, MCD_ESM_OLD);
+
+        // ------------------  AUCTION  ------------------
+
+        // Set CLIP for LINK-A in the DOG
+        DssExecLib.setContract(MCD_DOG, "LINK-A", "clip", MCD_CLIP_LINK_A);
+
+        // Authorize CLIP can access to VAT
+        DssExecLib.authorize(MCD_VAT, MCD_CLIP_LINK_A);
+
+        // Authorize CLIP can access to DOG
+        DssExecLib.authorize(MCD_DOG, MCD_CLIP_LINK_A);
+
+        // Authorize DOG can kick auctions on CLIP
+        DssExecLib.authorize(MCD_CLIP_LINK_A, MCD_DOG);
+
+        // We can't deauthorize the FLIP yet as there might be running auctions:
+        // DssExecLib.deauthorize(MCD_CAT, MCD_FLIP_LINK_A); TODO in a future spell
+
+        // No more auctions kicked via the CAT:
+        DssExecLib.deauthorize(MCD_FLIP_LINK_A, MCD_CAT);
+
+        // No more circuit breaker for the FLIP in LINK-A:
+        DssExecLib.deauthorize(MCD_FLIP_LINK_A, DssExecLib.flipperMom());
+
+        // Authorize the new END to access the LINK CLIP
+        DssExecLib.authorize(MCD_CLIP_LINK_A, MCD_END);
+
+        // Deauthorize the old END from all the FLIPS
+        // Authorize the new END in all the FLIPS
+        // (including MCD_FLIP_LINK_A as there might be still
+        // running auctions and shutdown could happen)
+        bytes32[] memory ilks = IlkRegistryAbstract(ILK_REGISTRY_OLD).list();
+        for (uint256 i = 0; i < ilks.length; i++) {
+            address flip = IlkRegistryAbstract(ILK_REGISTRY_OLD).flip(ilks[i]);
+            DssExecLib.deauthorize(flip, MCD_END_OLD);
+            DssExecLib.authorize(flip, MCD_END);
+        }
+        // DssExecLib.deauthorize(MCD_FLIP_LINK_A, MCD_END); TODO in a future spell
+
+         // ------------------  CHAINLOG  -----------------
+
+        DssExecLib.setChangelogAddress("MCD_DOG", MCD_DOG);
+        DssExecLib.setChangelogAddress("MCD_END", MCD_END);
+        DssExecLib.setChangelogAddress("MCD_ESM", MCD_ESM);
+        DssExecLib.setChangelogAddress("MCD_CLIP_LINK_A", MCD_CLIP_LINK_A);
+        // DssExecLib.setChangelogAddress("ILK_REGISTRY", ILK_REGISTRY); TODO in the following spell
+        ChainlogAbstract(DssExecLib.LOG).removeAddress("MCD_FLIP_LINK_A");
+
+        // DssExecLib.setChangelogVersion("1.3.0");
     }
 
 }
