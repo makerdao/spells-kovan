cf4d63572225248ae9e47219e69aa053931eefb4
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-04-14 16:25:36 -0300

    Update RWA002 contracts + test

--- src/Kovan-DssSpell.t.sol
+++ src/Kovan-DssSpell.t.sol
@@ -19,6 +19,41 @@ interface SpellLike {
     function cast() external;
 }
 
+// Specific for this spell
+interface ManagerLike {
+    function dai() external view returns (address);
+    function daiJoin() external view returns (address);
+    function end() external view returns (address);
+    function gem() external view returns (address);
+    function owner() external view returns (address);
+    function urn() external view returns (address);
+    function vat() external view returns (address);
+    function vow() external view returns (address);
+    function draw(uint256) external;
+    function exit(uint256) external;
+    function join(uint256) external;
+    function lock(uint256) external;
+    function wipe(uint256) external;
+}
+
+interface EpochCoordinatorLike {
+    function closeEpoch() external;
+    function currentEpoch() external returns(uint);
+}
+
+interface Root {
+    function relyContract(address, address) external;
+}
+
+interface MemberList {
+    function updateMember(address, uint) external;
+}
+
+interface AssessorLike {
+    function calcSeniorTokenPrice() external returns (uint);
+}
+//
+
 contract DssSpellTest is DSTest, DSMath {
 
     struct SpellValues {
@@ -99,7 +134,7 @@ contract DssSpellTest is DSTest, DSMath {
     JugAbstract              jug = JugAbstract(        addr.addr("MCD_JUG"));
     SpotAbstract         spotter = SpotAbstract(       addr.addr("MCD_SPOT"));
     DaiAbstract              dai = DaiAbstract(        addr.addr("MCD_DAI"));
-    DaiJoinAbstract      daiJoin = DaiJoinAbstract(    addr.addr("MCD_DAI_JOIN"));
+    DaiJoinAbstract      daiJoin = DaiJoinAbstract(    addr.addr("MCD_JOIN_DAI"));
     DSTokenAbstract          gov = DSTokenAbstract(    addr.addr("MCD_GOV"));
     EndAbstract              end = EndAbstract(        addr.addr("MCD_END"));
     ESMAbstract              esm = ESMAbstract(        addr.addr("MCD_ESM"));
@@ -115,7 +150,13 @@ contract DssSpellTest is DSTest, DSMath {
     FaucetAbstract        faucet = FaucetAbstract(     addr.addr("FAUCET"));
 
     // Specific for this spell
-    ESMAbstract        oldEsmBug = ESMAbstract(        0x0798603b8AE1f76D2823aDbc2E15d047Eac1Efd7);
+    ESMAbstract            oldEsmBug = ESMAbstract(        0x0798603b8AE1f76D2823aDbc2E15d047Eac1Efd7);
+    Root                        root = Root(0x25dF507570c8285E9c8E7FFabC87db7836850dCd);
+    MemberList            memberlist = MemberList(0xD927F069faf59eD83A1072624Eeb794235bBA652);
+    EpochCoordinatorLike coordinator = EpochCoordinatorLike(0xB51D3cbaa5CCeEf896B96091E69be48bCbDE8367);
+    address          seniorOperator_ = 0x6B902D49580320779262505e346E3f9B986e99e8;
+    address           seniorTranche_ = 0xDF0c780Ae58cD067ce10E0D7cdB49e92EEe716d9;
+    address                assessor_ = 0x49527a20904aF41d1cbFc0ba77576B9FBd8ec9E5;
     //
 
     DssSpell spell;
@@ -1712,6 +1753,59 @@ contract DssSpellTest is DSTest, DSMath {
         assertTrue(totalGas <= 8 * MILLION);
     }
 
+    function testRWA002() public {
+        ManagerLike dropMgr = ManagerLike(address(addr.addr("RWA002_A_INPUT_CONDUIT")));
+        DSTokenAbstract drop = DSTokenAbstract(address(dropMgr.gem()));
+
+        assertEq(dropMgr.urn(), address(addr.addr("RWA002_A_URN")));
+        assertEq(dropMgr.vat(), address(vat));
+        assertEq(dropMgr.vow(), address(vow));
+        assertEq(dropMgr.dai(), address(dai));
+        assertEq(dropMgr.daiJoin(), address(daiJoin));
+        assertEq(dropMgr.end(), address(end));
+
+        // welcome to hevm KYC
+        hevm.store(address(root), keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
+
+        root.relyContract(address(memberlist), address(this));
+        memberlist.updateMember(address(this), uint(-1));
+        memberlist.updateMember(address(dropMgr), uint(-1));
+
+        // set this contract as owner of dropMgr // override slot 1
+        // check what's inside slot 1 with: bytes32 slot = hevm.load(address(dropMgr), bytes32(uint(1)));
+        hevm.store(address(dropMgr), bytes32(uint(1)), bytes32(0x0000000000000000000101013bE95e4159a131E56A84657c4ad4D43eC7Cd865d));
+        // ste this contract as ward on the mgr
+        hevm.store(address(dropMgr), keccak256(abi.encode(address(this), uint(0))), bytes32(uint(1)));
+
+        // give this address 1500 dai and 1000 drop
+        hevm.store(address(dai), keccak256(abi.encode(address(this), uint(2))), bytes32(uint(1500 ether)));
+        hevm.store(address(drop), keccak256(abi.encode(address(this), uint(8))), bytes32(uint(1000 ether)));
+        assertEq(dai.balanceOf(address(this)), 1500 ether);
+        assertEq(drop.balanceOf(address(this)), 1000 ether);
+
+        // approve the manager
+        drop.approve(address(dropMgr), uint(-1));
+        dai.approve(address(dropMgr), uint(-1));
+
+        //execute spell and lock rwa token
+        vote(address(spell));
+        scheduleWaitAndCast(address(spell));
+        assertTrue(spell.done());
+        dropMgr.lock(1 ether);
+
+        assertEq(dai.balanceOf(address(this)), 1500 ether);
+        assertEq(drop.balanceOf(address(this)), 1000 ether);
+
+        dropMgr.join(400 ether);
+        dropMgr.draw(200 ether);
+        assertEq(dai.balanceOf(address(this)), 1700 ether);
+        assertEq(drop.balanceOf(address(this)), 600 ether);
+        assertEq(drop.balanceOf(address(dropMgr)), 400 ether);
+        dropMgr.wipe(10 ether);
+        dropMgr.exit(10 ether);
+        assertEq(dai.balanceOf(address(this)), 1690 ether);
+        assertEq(drop.balanceOf(address(this)), 610 ether);
+    }
 
     function testFailFireOldESMBug() public {
         vote(address(spell));
