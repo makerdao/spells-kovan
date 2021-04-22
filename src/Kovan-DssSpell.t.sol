057dce6acc9f506f17420ccc81f45a765d27ebb3
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-04-22 11:17:04 -0300

    Test with deployed spell

--- src/Kovan-DssSpell.t.sol
+++ src/Kovan-DssSpell.t.sol
@@ -240,8 +240,8 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1618926672,                 // use get-created-timestamp.sh if deployed
+            deployed_spell:                 0x18062f99D35AB057ca85506dadBF6015e20E0555,        // populate with deployed spell if deployed
+            deployed_spell_created:         1619100860,                 // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             previous_spell_execution_time:  1614790361,                 // Time to warp to in order to allow the previous spell to be cast ignored if PREV_SPELL is SpellLike(address(0)).
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
