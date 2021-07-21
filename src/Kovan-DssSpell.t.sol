5a1c66c99a4194501ce495fcfd15dd471058a65b
Jeroen Offerijns <Offerijns@users.noreply.github.com>
2021-07-21 20:30:52 +0200

    Update src/Kovan-DssSpell.t.sol
    Co-authored-by: Christopher Mooney <chris@dod.net>

--- src/Kovan-DssSpell.t.sol
+++ src/Kovan-DssSpell.t.sol
@@ -247,8 +247,8 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1623784720,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xdf7AC882DbbEbD9814d61F2b66afa6Dd4523D139),        // populate with deployed spell if deployed
+            deployed_spell_created:         1626887324,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           true,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
